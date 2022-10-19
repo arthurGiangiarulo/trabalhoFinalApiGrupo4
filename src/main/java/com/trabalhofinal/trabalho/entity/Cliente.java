4e825e4636a6d36e0fff659959c18925189adc13
Arthur Giangiarulo <emaildogiango@gmail.com>
2022-10-19 20:21:16 -0300

    Anotações das classes

--- src/main/java/com/trabalhofinal/trabalho/entity/Cliente.java
+++ src/main/java/com/trabalhofinal/trabalho/entity/Cliente.java
@@ -1,7 +1,12 @@
 package com.trabalhofinal.trabalho.entity;
 
+import javax.persistence.Column;
+
 public class Cliente {
     
+    //@Column(name = "nomedacoluna", unique = true)
+    //Esta anotação não permite valor repetido
+
     //Não permitir CPF repetido
     //Não permitir email repetido
 
