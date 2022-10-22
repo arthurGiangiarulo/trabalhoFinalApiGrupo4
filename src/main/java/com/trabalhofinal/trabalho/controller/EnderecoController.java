335259452a0919d6070f17edc34a83c399816c7a
Bernardo2459 <bernardodeoliveirasouza9@gmail.com>
2022-10-21 21:32:12 -0300

    delete de endereco

--- src/main/java/com/trabalhofinal/trabalho/controller/EnderecoController.java
+++ src/main/java/com/trabalhofinal/trabalho/controller/EnderecoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.trabalhofinal.trabalho.dto.ClienteDTO;
 import com.trabalhofinal.trabalho.dto.EnderecoDTO;
 import com.trabalhofinal.trabalho.service.EnderecoService;
 
@@ -75,5 +77,15 @@ public class EnderecoController {
 	public ResponseEntity<EnderecoDTO> updateEditoraDTO(@RequestBody EnderecoDTO enderecoDTO,
 			@PathVariable Integer id) {
 		return new ResponseEntity<>(enderecoService.update(enderecoDTO, id), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/dto/delete")
+	public ResponseEntity<EnderecoDTO> deleteClienteDTO(int id) {
+		EnderecoDTO enderecoDTO = enderecoService.getById(id);
+		if (enderecoDTO == null) {
+			return new ResponseEntity<>(enderecoDTO, HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<>(enderecoService.delete(id), HttpStatus.OK);
+		}
 	}
 }
