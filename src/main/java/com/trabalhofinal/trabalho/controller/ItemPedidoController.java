d59c40ce92ba2a3c0b7dbb1ac95f43701adb71e7
wellington-santos <wellingtonsantosceo@gmail.com>
2022-10-22 14:59:45 -0300

    bug-delete nos controles

--- src/main/java/com/trabalhofinal/trabalho/controller/ItemPedidoController.java
+++ src/main/java/com/trabalhofinal/trabalho/controller/ItemPedidoController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import com.trabalhofinal.trabalho.dto.ItemPedidoDTO;
 import com.trabalhofinal.trabalho.service.ItemPedidoService;
 
@@ -23,41 +22,14 @@ import com.trabalhofinal.trabalho.service.ItemPedidoService;
 public class ItemPedidoController {
 	@Autowired
 	ItemPedidoService itemService;
-	
-////CONTROLLER DAS ENTITY --------------------------------------------------------------------------------
-//	@GetMapping
-//   public ResponseEntity<List<Categoria>> getAllCategoria(){
-//       return new ResponseEntity<>(categoriaService.getAllCategoria(), HttpStatus.OK);
-//   }
-//	
-//	@GetMapping("/{id}")
-//	public ResponseEntity<Categoria> getCategoriaById(@PathVariable int id) {
-//		Categoria categoria = categoriaService.getCategoriaById(id);
-//		if (categoria != null) {
-//			return new ResponseEntity<>(categoria, HttpStatus.OK);
-//		} else {
-//			return new ResponseEntity<>(categoria, HttpStatus.NOT_FOUND);
-//		}
-//	}
-//	
-//	@PostMapping
-//	public ResponseEntity<Categoria> saveCategoria(@RequestBody Categoria categoria) {
-//		return new ResponseEntity<>(categoriaService.saveCategoria(categoria), HttpStatus.OK);
-//	}
-//	
-//	@PutMapping("/{id}")
-//	public ResponseEntity<Categoria> updateCategoria(@RequestBody Categoria categoria, @PathVariable int id) {
-//		return new ResponseEntity<>(categoriaService.updateCategoria(categoria, id), HttpStatus.OK);		
-//	}
-////FIM DO CONTROLLER DAS ENITY -----------------------------------------------------------------------------------
 
-//CONTROLLER DOS DTO
-	@GetMapping("/dto") 
-   public ResponseEntity<List<ItemPedidoDTO>> getAll(){
-       return new ResponseEntity<>(itemService.getAll(), HttpStatus.OK);
-   } 
-	
-	@GetMapping("/dto/{id}")
+//CONTROLLER DOS DTO's
+	@GetMapping("/search")
+	public ResponseEntity<List<ItemPedidoDTO>> getAll() {
+		return new ResponseEntity<>(itemService.getAll(), HttpStatus.OK);
+	}
+
+	@GetMapping("/search/id/{id}")
 	public ResponseEntity<ItemPedidoDTO> getById(@PathVariable int id) {
 		ItemPedidoDTO itemDTO = itemService.getById(id);
 		if (itemDTO != null) {
@@ -66,19 +38,19 @@ public class ItemPedidoController {
 			return new ResponseEntity<>(itemDTO, HttpStatus.NOT_FOUND);
 		}
 	}
-	
-	@PostMapping("/dto")
-   public ResponseEntity<ItemPedidoDTO> save(@RequestBody ItemPedidoDTO enderecoDTO) {
-       return new ResponseEntity<>(itemService.save(enderecoDTO), HttpStatus.CREATED);
-   }
-	
-	@PutMapping("/dto/{id}")
+
+	@PostMapping("/save")
+	public ResponseEntity<ItemPedidoDTO> save(@RequestBody ItemPedidoDTO enderecoDTO) {
+		return new ResponseEntity<>(itemService.save(enderecoDTO), HttpStatus.CREATED);
+	}
+
+	@PutMapping("/update/{id}")
 	public ResponseEntity<ItemPedidoDTO> updateEditoraDTO(@RequestBody ItemPedidoDTO itemDTO,
 			@PathVariable Integer id) {
 		return new ResponseEntity<>(itemService.update(itemDTO, id), HttpStatus.OK);
 	}
-	
-	@DeleteMapping("/dto/delete")
+
+	@DeleteMapping("/delete/{id}")
 	public ResponseEntity<ItemPedidoDTO> deleteItemDTO(int id) {
 		ItemPedidoDTO itemDTO = itemService.getById(id);
 		if (itemDTO == null) {
